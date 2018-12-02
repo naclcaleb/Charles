f1e10be2a4298de68f5605c15478ad69035e03f3
Caleb H <42253012+naclcaleb@users.noreply.github.com>
2018-12-01 16:29:53 -0800

    Update Indicator.py

--- EXPERIMENTAL/devices/pi_zero/Charles_core/Indicator.py
+++ EXPERIMENTAL/devices/pi_zero/Charles_core/Indicator.py
@@ -1,3 +1,11 @@
 import RPi.GPIO
 class Indicator:
-  
+    def __init__(self):
+      pass
+    def listening(self):
+        #Make light pattern with connected LEDs, signifying that Charles is listening
+        pass
+    def processing(self):
+        #Make light pattern with connected LEDs, signifying that Charles is processing a request
+        pass
+    
