29006d22711fd8c2e0087aa5bb881477ca01e691
Caleb H <42253012+naclcaleb@users.noreply.github.com>
2018-12-01 16:24:35 -0800

    Update main.py

--- EXPERIMENTAL/devices/pi_zero/main.py
+++ EXPERIMENTAL/devices/pi_zero/main.py
@@ -1,1 +1,27 @@
 from Charles_core import *
+
+charles = Charles()
+light = Indicator()
+_sr = SpeechRecognizer()
+voice = Voice()
+
+nc = NetworkCheck()
+online = nc.check()
+print(online)
+
+#If wake word is detected, run processes
+def runNetwork():
+    light.on()
+    
+    text = _sr.recognize(online)
+    
+    response = charles.default_request(text)
+    
+    voice.speak(response)
+    
+    light.off()
+def test():
+    print("Detected")
+wake_word_detector = WakeWord()
+
+wake_word_detector.run(callback=runNetwork)
