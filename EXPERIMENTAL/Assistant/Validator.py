04e41471b074a9b1fa4a8bfdd992e9b368c3f14b
Caleb H <42253012+naclcaleb@users.noreply.github.com>
2018-12-01 20:43:31 -0800

    Added token validation

--- EXPERIMENTAL/Assistant/Validator.py
+++ EXPERIMENTAL/Assistant/Validator.py
@@ -1,1 +1,12 @@
 class Validator:
+    def __init__(self):
+        pass
+    def validate(self, token):
+        valid_tokens = []
+        with open("tokens.txt","r") as file:
+            txt = file.read()
+            valid_tokens = txt.split("\n")
+        if token in valid_tokens:
+            return True
+        else:
+            return False
