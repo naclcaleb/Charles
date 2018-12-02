8d48e92b295f59eed5a8c54aeea8a91f1a0567ed
Caleb H <42253012+naclcaleb@users.noreply.github.com>
2018-12-01 20:36:46 -0800

    Update api.py

--- EXPERIMENTAL/api.py
+++ EXPERIMENTAL/api.py
@@ -1,7 +1,22 @@
 from flask import Flask
 from flask import request
+from Assistant import *
 app = Flask(__name__)
 
-@app.route("/api/request_handler")
+validator = Validator()
+network = Seq2Seq()
+
+@app.route("/api/request_handler", methods=['POST','GET'])
 def give_response():
+    auth_token = request.form["api_key"]
+    req_txt = request.form["request"]
     
+    #If needed, we can handle different device types here
+    
+    if validator.validate(auth_token):
+        response = network.generateResponse(req_txt)
+        return response
+    else:
+        return "INVALID AUTH TOKEN"
+
+        
