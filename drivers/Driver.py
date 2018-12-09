cce6b5f108b137009c1d9eb7b1ed556a7ea341ad
Caleb H <42253012+naclcaleb@users.noreply.github.com>
2018-12-08 17:19:14 -0800

    Update Driver.py

--- drivers/Driver.py
+++ drivers/Driver.py
@@ -1,1 +1,15 @@
+from events import Registry
 class Driver:
+    def __init__(self, name):
+        self.name = name
+    def add_event_listener(self,event, function):
+        Registry.add(self.name, event, function)
+    def remove_event_listener(self, event):
+        Registry.remove(self.name,event)
+    def send(self, data):
+        pass
+    def get(self,data=[]):
+        pass
+    
+        
+    
