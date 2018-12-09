13c92549629bb7463eb52fb24c6563d84df92867
Caleb H <42253012+naclcaleb@users.noreply.github.com>
2018-12-08 17:49:53 -0800

    Update events.py

--- events.py
+++ events.py
@@ -1,1 +1,30 @@
+class Event:
+    def __init__(self, name):
+        self.name = name
+    def trigger(self):
+        try:
+            dispatch(self.name)
+            return True
+        else:
+            return False
+class Registry:
+    def __init__(self):
+        pass
+    def add(self,device, event, function):
+        handler.add_listener(device, event, function)
+    def remove(self, device,event):
+        handler.remove_listener(device,event)
+class Handler:
+    def __init__(self):
+        self.listeners = []
+    def add_listener(device, event, callback):
+        self.listeners.append({"device":device,"event":event,"callback":callback})
+    def remove_listener(device,event):
+        self.listeners = [ x for x in self.listeners if (x.device != device) or (x.event != event) ]
+    def handle(self,event):
+        for i in self.listeners:
+            if i.event == event:
+                i.callback()
+        
+          
 
